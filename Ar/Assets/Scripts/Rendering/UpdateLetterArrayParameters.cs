44302c61e943bd659605b2fcdfef954a8f42fa3b
Mamadsho <mamadsho.nazaramonov@gmail.com>
2026-05-09 19:38:23 +0300

    VFX Fixes

--- Ar/Assets/Scripts/Rendering/UpdateLetterArrayParameters.cs
+++ Ar/Assets/Scripts/Rendering/UpdateLetterArrayParameters.cs
@@ -6,6 +6,7 @@ public class UpdateLetterArrayParameters : MonoBehaviour
 {
     [SerializeField] float noiseAmplitude = 0.5f;
     [SerializeField] float noiseScale = 0.5f;
+    [SerializeField] float ySize = 1.0f;
 
     [SerializeField] [ColorUsage(false, true)]
     private Color color = new Color(0.0f, 6.79047918f, 14.3498688f, 1.0f);
@@ -22,6 +23,7 @@ public class UpdateLetterArrayParameters : MonoBehaviour
             letter.GetComponent<Renderer>().sharedMaterial.SetFloat("_NoiseAmplitude", noiseAmplitude);
             letter.GetComponent<Renderer>().sharedMaterial.SetFloat("_NoiseScale", noiseScale);
             letter.GetComponent<Renderer>().sharedMaterial.SetColor("_Color", color);
+            letter.GetComponent<Renderer>().sharedMaterial.SetFloat("_YSize", ySize);
             
         }
     }
