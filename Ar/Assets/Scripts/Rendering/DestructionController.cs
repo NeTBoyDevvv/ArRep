df6f160d7c67f882543fe929e0cf414ad88d0760
Mamadsho <mamadsho.nazaramonov@gmail.com>
2026-05-10 07:51:37 +0300

    VFX Fixes

--- Ar/Assets/Scripts/Rendering/DestructionController.cs
+++ Ar/Assets/Scripts/Rendering/DestructionController.cs
@@ -7,30 +7,33 @@ namespace Rendering
     public class DestructionController : MonoBehaviour
     {
         private Matrix4x4 lastMatrix;
+        private Material lastMaterial;
+        private Renderer _renderer;
         [SerializeField] private bool isWorldToLocal = true;
         [SerializeField] private GameObject[] destructables;
         [SerializeField] private string materialParameter = "_WorldToLocal";
 
-        void Awake()
-        {
-            lastMatrix = transform.worldToLocalMatrix;
-        }
     
         void OnValidate()
         {
             Update();
         }
+
+        void Start()
+        {
+            _renderer = GetComponent<Renderer>();
+        }
         
         void Update()
         {
-            if (transform.worldToLocalMatrix != lastMatrix)
-            {
+            //if (transform.worldToLocalMatrix != lastMatrix ) // TODO: Material change must be considered as well
+            //{
                 lastMatrix = isWorldToLocal? transform.worldToLocalMatrix : transform.localToWorldMatrix;
                 foreach (GameObject destructable in destructables)
                 {
                     destructable.GetComponent<Renderer>().sharedMaterial.SetMatrix(materialParameter , lastMatrix);
                 }
-            }
+            //}
         }
     
     }
